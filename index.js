f493e8e72d3e38a80607f5da6094693b4f686f95
Jackson Mooring <45497076+Gninoskcaj@users.noreply.github.com>
2019-05-21 11:45:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 
-module.exports.add = (a,b) => {
+const fs = require('fs');
+
+
+module.exports.add = (a, b) => {
 	return a + b;
 };
 
-module.exports.mult = (a,b) => {
+module.exports.mult = (a, b) => {
 	return a * b;
 };
 
-module.exports.divide = (a,b) => {
+module.exports.divide = (a, b) => {
 	return a / b;
 };
 
-module.exports.mod = (a,b) => {
+module.exports.mod = (a, b) => {
 	return a % b;
 };
 
-module.exports.percent = (a,b) => {
+module.exports.percent = (a, b) => {
 	return (a / b) * 100;
 };
 
 module.exports.addMany = (array) => {
 	var result = 0;
 	for (let i = 0; i < array.length; i++) {
-		 result +=array[i]
+		result += array[i]
 	}
 	return result;
 };
+
+module.exports.fileSize = (array, max) => {
+	let sizes = [];
+
+	for (x in array) {
+		sizes.push(getFilesizeInBytes(array[x]));
+	}
+	if (max) return module.exports.percent(module.exports.addMany(sizes), max);
+	else return module.exports.addMany(sizes);
+	
+	function getFilesizeInBytes(filename) {
+		var stats = fs.statSync(filename)
+		var fileSizeInBytes = stats["size"]
+		return fileSizeInBytes
+	}
+}
+
