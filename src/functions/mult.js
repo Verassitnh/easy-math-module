65eb8f8b9486e67fee80ebbf51cb33bdddf4c8aa
Jackson <jackson.mooring@gmail.com>
2019-05-23 12:18:11 -0500

    Make mult() accept an array

--- src/functions/mult.js
+++ src/functions/mult.js
@@ -1,6 +1,11 @@
 // Multiplication
-var mult = (a, b) => {
-	return a * b;
+let mult = (array) => {
+	var result = 0;
+	for (let i = 0; i < array.length; i++) {
+		let x = array[i];
+		result *= x;
+	}
+	return result;
 };
 
 module.exports = mult;
