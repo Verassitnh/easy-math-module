48172a6da654769e9e5b4b6998f1acccad163137
Jackson <jackson.mooring@gmail.com>
2019-05-23 09:40:49 -0500

    Change all ' to "

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const add = require('./src/functions/add');
-const subtract = require('./src/functions/subtract');
-const mult = require('./src/functions/mult');
-const mod = require('./src/functions/mod');
-const addMany = require('./src/functions/addMany');
-const percent = require('./src/functions/percent');
-const divide = require('./src/functions/divide');
-const fileSize = require('./src/functions/fileSize');
+const add = require("./src/functions/add");
+const subtract = require("./src/functions/subtract");
+const mult = require("./src/functions/mult");
+const mod = require("./src/functions/mod");
+const addMany = require("./src/functions/addMany");
+const percent = require("./src/functions/percent");
+const divide = require("./src/functions/divide");
+const fileSize = require("./src/functions/fileSize");
 
 module.exports.add = add;
 module.exports.subtract = subtract;
