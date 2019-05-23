45ff1df45d935df9d595dd392f194f73b1283ae1
Jackson <jackson.mooring@gmail.com>
2019-05-23 16:45:51 -0500

    Update mult.js  to docs

--- src/functions/mult.js
+++ src/functions/mult.js
@@ -1,9 +1,17 @@
 // Multiplication
-let mult = (array) => {
-	var result = 0;
-	for (let i = 0; i < array.length; i++) {
-		let x = array[i];
-		result *= x;
+let mult = (array, mult) => {
+	if (mult) {
+		let result = [];
+
+		for (let x in array) {
+			result.push(array[x] * mult)
+		}
+	} else {
+		var result = 0;
+		for (let i = 0; i < array.length; i++) {
+			let x = array[i];
+			result *= x;
+		}
 	}
 	return result;
 };
