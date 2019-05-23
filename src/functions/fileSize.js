81e85608a23178e0160713c651281a5b24155f37
Jackson <jackson.mooring@gmail.com>
2019-05-23 10:08:52 -0500

    Fix bad practice in fileSize()

--- src/functions/fileSize.js
+++ src/functions/fileSize.js
@@ -1,10 +1,50 @@
 const fs = require("fs");
 
 
-function fileSize(array, max) {
+// Helper functions
+function percent(a, b) {
+	return (a / b) * 100;
+}
 
-	// Local vars
+function addMany(array) {
+	var result = 0;
+	for (let i = 0; i < array.length; i++) {
+		result += array[i];
+	}
+	return result;
+}
+
+function getFilesizeInBytes(filename) {
+	var stats = fs.statSync(filename);
+	var fileSizeInBytes = stats["size"];
+	return fileSizeInBytes;
+}
+
+function parseMax(max) {
+
+	var finalNum = "1";
+	var num = Number(max.match(/\d*/)[0]);
+	var suffix = max.match(/[A-Z]+/)[0];
+
+	var array = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB",];
+
+	for (x in array) {
+		if (suffix === array[x]) {
+			var x3 = x * 3;
+			for (var i = 0; i < x3; i++) {
+				finalNum += "0";
+			}
+		}
+	}
+
+	var result = Number(finalNum);
+	result *= num;
+	return result;
+}
+
+function fileSize(array, max) {
 	let sizes = [];
+
 	try {
 		if (max) {
 			max = parseMax(max);
@@ -24,47 +64,7 @@ function fileSize(array, max) {
 
 
 
-	// Helper functions
-	function percent(a, b) {
-		return (a / b) * 100;
-	}
-
-	function addMany(array) {
-		var result = 0;
-		for (let i = 0; i < array.length; i++) {
-			result += array[i];
-		}
-		return result;
-	}
-
-	function getFilesizeInBytes(filename) {
-		var stats = fs.statSync(filename);
-		var fileSizeInBytes = stats["size"];
-		return fileSizeInBytes;
-	}
-
-	function parseMax(max) {
-
-
-		var finalNum = "1";
-		var num = Number(max.match(/\d*/)[0]);
-		var suffix = max.match(/[A-Z]+/)[0];
-
-		var array = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB",];
-
-		for (x in array) {
-			if (suffix === array[x]) {
-				var x3 = x * 3;
-				for (var i = 0; i < x3; i++) {
-					finalNum += "0";
-				}
-			}
-		}
-
-		var result = Number(finalNum);
-		result *= num;
-		return result;
-	}
+	
 }
 
 
